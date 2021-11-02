4ebbf26add3de7cf1ccb3da6ca85daa631e62e6a
N'CHE <nche.song@gmail.com>
2021-11-02 13:09:45 +0900

    Create a video upload template

--- client/src/components/views/VideoUploadPage/VideoUploadPage.js
+++ client/src/components/views/VideoUploadPage/VideoUploadPage.js
@@ -6,7 +6,61 @@ const { Title } = Typography;
 
 function VideoUploadPage() {
     return (
-        <div>VideoUploadPage</div>
+        <div style={{maxWidth: '700px', margin: '2rem auto'}}>
+            <div style={{textAlign: 'center', marginBottom: '2rem'}}>
+                <Title level={2}>Upload Video</Title>
+            </div>
+
+            <Form onSubmit>
+                <div style={{ display: 'flex', justifyContent: 'space-between'}}>
+                    {/* Drop zone */}
+                    {/* Thumbnail */}
+                    <div>
+                        <img src alt />
+                    </div>
+                </div>
+
+                <br/>
+                <br/>
+
+                <label>Title</label>
+                <Input
+                    onChange
+                    value
+                />
+
+                <br/>
+                <br/>
+
+                <label>Description</label>
+                <TextArea
+                    onChange
+                    value
+                />
+
+                <br/>
+                <br/>
+
+                <select onChange>
+                    <option></option>
+                </select>
+
+                <br/>
+                <br/>
+
+                <select onChange>
+                    <option></option>
+                </select>
+
+                <br/>
+                <br/>
+
+                <Button type="primary" size="large" onClick>
+                    Submit
+                </Button>
+
+            </Form>
+        </div>
     )
 }
 
