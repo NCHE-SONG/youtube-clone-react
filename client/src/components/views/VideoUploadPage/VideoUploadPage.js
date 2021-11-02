af22b4c3be6d5c555b0eac925ac6bb9c2db131af
N'CHE <nche.song@gmail.com>
2021-11-02 13:09:12 +0900

    Import Ant Design

--- client/src/components/views/VideoUploadPage/VideoUploadPage.js
+++ client/src/components/views/VideoUploadPage/VideoUploadPage.js
@@ -1,4 +1,9 @@
 import React from "react";
+import { Typography, Button, Form, message, Input, Icon} from "antd";
+
+const { TextArea } = Input;
+const { Title } = Typography;
+
 function VideoUploadPage() {
     return (
         <div>VideoUploadPage</div>
