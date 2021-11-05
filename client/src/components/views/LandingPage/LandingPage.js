5e5a1a96a41c95f370397c832550ef5e2824a7d9
N'CHE <nche.song@gmail.com>
2021-11-05 13:25:08 +0900

    Create a blank landing page

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -3,13 +3,16 @@ import { FaCode } from "react-icons/fa";
 
 function LandingPage() {
     return (
-        <>
-            <div className="app">
-                <FaCode style={{ fontSize: '4rem' }} /><br />
-                <span style={{ fontSize: '2rem' }}>Let's Start Coding!</span>
-            </div>
-            <div style={{ float: 'right' }}>Thanks For Using This Boiler Plate by John Ahn</div>
-        </>
+        <div style={{ width: '85%', margin: '3rem auto'}}>
+            <Title level={2}> Recommended </Title>
+            <br />
+            <Row gutter={[32, 16]}>
+
+                {renderCards}
+
+            </Row>
+
+        </div>
     )
 }
 
