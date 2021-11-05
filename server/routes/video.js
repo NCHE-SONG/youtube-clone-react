d49bbf611f42a97f1e62ce571ff50877f17ee434
N'CHE <92004625+NCHE-SONG@users.noreply.github.com>
2021-11-05 13:29:41 +0900

    Show videos on landing page (#28)
    * Modify the noSpace function to replace spaces from "" to "_"
    * Create a Video Collection
    * Create a 'onSubmit' Function
    * Send a request data to the server
    * Save data in the MongoDB
    * Move to '~/' page after 1 second when video upload is successful
    * Show videos on landing page #5 (#27)
    * Create a blank landing page
    * Create a video card template
    * Get all video data from mongoDB
    * Fix variable typo
    * Modify thumbnail count

--- server/routes/video.js
+++ server/routes/video.js
@@ -48,13 +48,24 @@ router.post('/uploads', (req, res) => {
 router.post('/uploadVideo', (req, res) => {
     // 인스턴스화 후 정보 저장
     const video = new Video(req.body)
-
+    
     // 비디오 정보 DB에 저장
     video.save((err, doc) => {
         if(err) return res.json({success: false, err})
         res.status(200).json({success: true})
     })
 
+})
+
+router.get('/getVideos', (req, res) => {
+    // DB에서 비디오를 가져와 클라이언트에 보낸다.
+    //video collection 모든 documents 가져옴.
+    Video.find()
+        .populate('writer')
+        .exec((err, videos) => {
+            if(err) return res.status(400).send(err);
+            res.status(200).json({ success: true, videos })
+        })
 })
 
 router.post('/thumbnails', (req, res) => {
@@ -70,17 +81,16 @@ router.post('/thumbnails', (req, res) => {
     // 썸네일 생성
     ffmpeg(req.body.url)
         .on('filenames', function (filenames) {
-            console.log("filenames", filenames)
             filePath = "thumbnails/" + filenames[0]
         })
         .on('end', function (){
-            return res.json({success: true, url: filePath, fileDuration: fileDuration})
+            return res.json({success: true, url: filePath, duration: fileDuration})
         })
         .on('error', function (err) {
             return res.json({success: false, err})
         })
         .screenshots({
-            count: 3,
+            count: 1,
             folder: 'thumbnails',
             size: '320x240',
             filename: 'thumbnail-%b.png'
