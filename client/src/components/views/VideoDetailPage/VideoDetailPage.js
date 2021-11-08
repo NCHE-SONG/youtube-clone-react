b040c5cbbeee2aa4cd4f5e57c93d2db7b4c22171
N'CHE <nche.song@gmail.com>
2021-11-08 17:36:35 +0900

    Create a video detail page template

--- client/src/components/views/VideoDetailPage/VideoDetailPage.js
+++ client/src/components/views/VideoDetailPage/VideoDetailPage.js
@@ -1,10 +1,47 @@
 import React from 'react'
-
+import {Row, Col, List} from 'antd'
+import Axios from 'axios'
+import {response} from "express";
 function VideoDetailPage() {
+
+    useEffect(() =>{
+        const videoId = props.match.params.videoId //App.js Route uri :  video/:videoId
+        const variable = {videoId: videoId}
+        Axios.post('/api/video/getVideoDetail', variable)
+            .then(response => {
+                if(response.data.success){
+
+                } else {
+                    alert('Failed to get a video data')
+                }
+            })
+    }, [input])
+
     return (
-        <div>
-            VideoDetailpage
-        </div>
+        <Row gutter={[16, 16]}>
+            <Col lg={18} xs={24}>
+                <div style={{ width: '100%', padding: '3rem rem' }}>
+                    <video style={{width: '100%'}} src controls />
+
+                    <List.Item
+                        actions
+                    >
+                        <List.Item.Meta
+                            avatar
+                            title
+                            description
+                            />
+                    </List.Item>
+
+                    {/* */}
+
+                </div>
+
+            </Col>
+            <Col lg={6} xs={24}>
+                Side Videos
+            </Col>
+        </Row>
     )
 }
 
