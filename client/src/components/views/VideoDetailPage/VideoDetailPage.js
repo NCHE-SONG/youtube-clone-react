6a14ad357c1a368bef6c6db235e47cc339edb55a
N'CHE <92004625+NCHE-SONG@users.noreply.github.com>
2021-11-09 15:52:23 +0900

    #34 Not appear the currently playing video in the sub video list (#36)
    Merge pull request #36
    * Remove log
    * Add property
    * Not appear the currently playing video in the list

--- client/src/components/views/VideoDetailPage/VideoDetailPage.js
+++ client/src/components/views/VideoDetailPage/VideoDetailPage.js
@@ -1,22 +1,18 @@
-import React, { useEffect, useState } from 'react';
-import {Row, Col, List, Avatar} from 'antd';
-import Axios from 'axios';
-
+import React, { useEffect, useState } from 'react'
+import {Row, Col, List, Avatar} from 'antd'
+import Axios from 'axios'
 import SideVideo from './Sections/SideVideo'
 
 function VideoDetailPage(props) {
 
     const videoId = props.match.params.videoId //App.js Route uri :  video/:videoId
     const [VideoDetail, setVideoDetail] = useState([])
-
-
     const variable = {videoId: videoId}
 
     useEffect(() =>{
         Axios.post('/api/video/getVideoDetail', variable)
             .then(response => {
                 if(response.data.success){
-                    console.log('Hello ~ useEffect', response.data.video)
                     setVideoDetail(response.data.video)
                 } else {
                     alert('Failed to get a video info')
@@ -47,15 +43,13 @@ function VideoDetailPage(props) {
 
                 </Col>
                 <Col lg={6} xs={24}>
-                    <SideVideo />
+                    <SideVideo videoId={videoId} />
                 </Col>
             </Row>
         )
     }
 
     return <div>"loading..."</div>
-
-
 
 }
 
