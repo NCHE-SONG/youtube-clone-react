1472d089fee87eb4cf1f557576cc8fdf476e1bb2
N'CHE <nche.song@gmail.com>
2021-11-09 16:29:27 +0900

    Change statement
    - Change `if` statement to ternary operator
    - Change `props.match.params.videoId` to `useParams()`

--- client/src/components/views/VideoDetailPage/VideoDetailPage.js
+++ client/src/components/views/VideoDetailPage/VideoDetailPage.js
@@ -2,15 +2,16 @@ import React, { useEffect, useState } from 'react'
 import {Row, Col, List, Avatar} from 'antd'
 import Axios from 'axios'
 import SideVideo from './Sections/SideVideo'
+import {useParams} from "react-router-dom";
 
-function VideoDetailPage(props) {
+function VideoDetailPage() {
 
-    const videoId = props.match.params.videoId //App.js Route uri :  video/:videoId
+    const params = useParams();
+    const videoId = params.videoId //App.js Route uri :  video/:videoId
     const [VideoDetail, setVideoDetail] = useState([])
-    const variable = {videoId: videoId}
 
     useEffect(() =>{
-        Axios.post('/api/video/getVideoDetail', variable)
+        Axios.post('/api/video/getVideoDetail', {videoId : videoId})
             .then(response => {
                 if(response.data.success){
                     setVideoDetail(response.data.video)
@@ -20,8 +21,8 @@ function VideoDetailPage(props) {
             })
     }, [])
 
-    if(VideoDetail.writer){
-        return (
+    return (
+        VideoDetail.writer ?
             <Row gutter={[16, 16]}>
                 <Col lg={18} xs={24}>
                     <div style={{ width: '100%', padding: '3rem rem' }}>
@@ -46,10 +47,8 @@ function VideoDetailPage(props) {
                     <SideVideo videoId={videoId} />
                 </Col>
             </Row>
-        )
-    }
-
-    return <div>"loading..."</div>
+            : <div>"loading..."</div>
+    )
 
 }
 
