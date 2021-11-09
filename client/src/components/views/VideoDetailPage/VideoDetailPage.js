d23325ea0ed4bb056eba0504a2102ec8bd1f46fa
N'CHE <nche.song@gmail.com>
2021-11-09 11:27:36 +0900

    Create side video layout template

--- client/src/components/views/VideoDetailPage/VideoDetailPage.js
+++ client/src/components/views/VideoDetailPage/VideoDetailPage.js
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from 'react';
 import {Row, Col, List, Avatar} from 'antd';
 import Axios from 'axios';
 
+import SideVideo from './Sections/SideVideo'
+
 function VideoDetailPage(props) {
 
     const videoId = props.match.params.videoId //App.js Route uri :  video/:videoId
@@ -39,13 +41,13 @@ function VideoDetailPage(props) {
                             />
                         </List.Item>
 
-                        {/* */}
+                        {/* Comments */}
 
                     </div>
 
                 </Col>
                 <Col lg={6} xs={24}>
-                    Side Videos
+                    <SideVideo />
                 </Col>
             </Row>
         )
