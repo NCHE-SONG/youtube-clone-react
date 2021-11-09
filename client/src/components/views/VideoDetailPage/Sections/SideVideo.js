bec99545e7655cd657497cb0182f8668a585c951
N'CHE <nche.song@gmail.com>
2021-11-09 15:49:07 +0900

    Not appear the currently playing video in the list

--- client/src/components/views/VideoDetailPage/Sections/SideVideo.js
+++ client/src/components/views/VideoDetailPage/Sections/SideVideo.js
@@ -1,9 +1,10 @@
 import React, {useEffect, useState} from 'react';
 import Axios from "axios";
 
-function SideVideo() {
+function SideVideo(props) {
 
-    const [sideVideos, setSideVideos] = useState([]);
+    const currentVideoId = props.videoId
+    const [sideVideos, setSideVideos] = useState([])
 
     useEffect(() => {
         Axios.get('/api/video/getVideos')
@@ -17,27 +18,28 @@ function SideVideo() {
     }, [])
 
     const renderSideVideo = sideVideos.map((video, index) => {
+        if(currentVideoId !== video._id){
+            var minutes = Math.floor(video.duration / 60);
+            var seconds = Math.floor(video.duration - minutes * 60);
 
-        var minutes = Math.floor(video.duration / 60);
-        var seconds = Math.floor(video.duration - minutes * 60);
+            return <div key={index} style={{display: 'flex', marginTop: '1rem', padding: '0 2rem'}}>
+                <div style={{width: '40%', marginRight: '1rem'}}>
+                    <a href={`/video/${video._id}`} style={{color: 'gray'}}>
+                        <img style={{width: '100%', height: '100%'}} src={`http://localhost:5000/${video.thumbnail}`} alt='thumbnail' />
+                    </a>
+                </div>
 
-        return <div key={index} style={{display: 'flex', marginTop: '1rem', padding: '0 2rem'}}>
-            <div style={{width: '40%', marginRight: '1rem'}}>
-                <a href={`/video/${video._id}`} style={{color: 'gray'}}>
-                    <img style={{width: '100%', height: '100%'}} src={`http://localhost:5000/${video.thumbnail}`} alt='thumbnail' />
-                </a>
+                <div style={{width: '50%'}}>
+                    <a href={`/video/${video._id}`} style={{color : 'gray'}}>
+                        <span style={{ fontSize: '1rem', color: 'black'}}> {video.title}</span><br />
+                        <span>{video.writer.name}</span><br />
+                        <span>{video.views} views</span><br />
+                        <span>{minutes} : {seconds}</span>
+                    </a>
+                </div>
+
             </div>
-
-            <div style={{width: '50%'}}>
-                <a href={`/video/${video._id}`} style={{color : 'gray'}}>
-                    <span style={{ fontSize: '1rem', color: 'black'}}> {video.title}</span><br />
-                    <span>{video.writer.name}</span><br />
-                    <span>{video.views} views</span><br />
-                    <span>{minutes} : {seconds}</span>
-                </a>
-            </div>
-
-        </div>
+        }
     })
 
     return (
